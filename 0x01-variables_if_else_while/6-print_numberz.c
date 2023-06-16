35430c91b3a1fe0c94ac4d6bce7530f5b12f3860
Oluwagbemide_Fikayo <d.fikayo@yahoo.com>
2023-06-16 16:31:36 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+/* prints all single digits using putchar */
+int main(void)
+{
+	int m;
+	
+	for (m = 0; m <= 9; m++)
+	{
+		putchar(m + '0');
+	}
+	putchar('\n');
+		return (0);
+}
