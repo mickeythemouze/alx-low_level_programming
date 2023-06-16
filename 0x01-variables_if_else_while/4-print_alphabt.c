8ddd30425805b05020340d4e6433526b6d6cadb6
Oluwagbemide_Fikayo <d.fikayo@yahoo.com>
2023-06-16 15:28:17 -0700

    alphabets without q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,8 +10,11 @@ int main(void)
 {
 	char al;
 	{
-	for (al = 'a'; ((al != 'q') && (al != 'e')) && (al <= 'z'); al++)
+	for (al = 'a'; (al <= 'z'); al++)
+		if (al != 'q' && al != 'e')
+		{
 		putchar(al);
+	}
 	}
 		putchar('\n');
 	return (0);
