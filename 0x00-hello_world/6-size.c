2563d3b443892341f56567bde914a04bd831c64f
Oluwagbemide_Fikayo <d.fikayo@yahoo.com>
2023-06-15 22:08:12 -0700

    corrections

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,14 +11,14 @@ int main(void)
 	int b;
 	long int c;
 	long long int d;
-	float;e
+	float e;
 
 	/*size of variable types*/
-	printf("Size of a char: %lu byte(s)\n", sizeof(a));
-	printf("Size of an int: %lu byte(s)\n", sizeof(b));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(c);
-	printf("Size of a long long int: %lu byte(s)\n" sizeof(d);
-	Printf("Size of a float: %lu byte(s) \n" sizeof(e);		
+	printf("Size of a char: %2d byte(s)\n", sizeof(a));
+	printf("Size of an int: %2d byte(s)\n", sizeof(b));
+	printf("Size of a long int: %2d byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %2d byte(s)\n" sizeof(d));
+	Printf("Size of a float: %2d byte(s)\n" sizeof(e));		
 	return (0);
 
 }
