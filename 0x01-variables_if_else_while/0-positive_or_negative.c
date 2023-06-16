5fbe92ea8ef81b26550d1e7db181bd9d6ee764f8
Oluwagbemide_Fikayo <d.fikayo@yahoo.com>
2023-06-16 08:06:22 -0700

    Random numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
-/* betty style doc for function main goes there */
+/* This program will assign a random number to the variable n each time it is executed */
 int main(void)
 {
 	int n;
