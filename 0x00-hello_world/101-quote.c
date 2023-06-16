67bb514813eb2f30b23396e076399cad67b74d2d
Oluwagbemide_Fikayo <d.fikayo@yahoo.com>
2023-06-15 21:53:28 -0700

    corrections

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,11 +4,12 @@
 /**
  * main- A program that prints to the standard error
  *
- * return- (1) Success
+ * Return- Always 1 (Success)
  */
 
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+
+	return 1;
 }
