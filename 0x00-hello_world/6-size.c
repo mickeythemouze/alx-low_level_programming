110cb38232df7bdb0d63d5c0370c0ea96e48c7c3
Oluwagbemide_Fikayo <d.fikayo@yahoo.com>
2023-06-15 22:21:30 -0700

    correction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-	char a;
+	char j;
 	int b;
 	long int c;
 	long long int d;
 	float e;
 
 	/*size of variable types*/
-	printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(a));
+	printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(j));
 	printf("Size of an int: %lu byte(s)\n",(unsigned long)sizeof(b));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a long long int: %lu byte(s)\n" (unsigned long)sizeof(d));
