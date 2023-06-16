a15d5ac34986f40cce2bad53185ec0f79170dd7c
Oluwagbemide_Fikayo <d.fikayo@yahoo.com>
2023-06-16 14:27:19 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Lowercase Alphabets
+ *
+ * Return: Always 0 (Success)
+*/
+
+/* Printing alphabets in lower case */
+int main(void)
+{
+	char al = 'a';
+	for (al = 'a';al <= 'z'; al++)
+		putchar(al);
+		putchar('\n');
+	return (0);
+}
