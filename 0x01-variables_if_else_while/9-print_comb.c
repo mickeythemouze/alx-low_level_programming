0817a1b2d60296786bb1daf8f67d564783388031
Oluwagbemide_Fikayo <d.fikayo@yahoo.com>
2023-06-16 17:52:31 -0700

    prints infite comb of single digit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+/*Prints all infite set of single digit numbers*/
+int main(void)
+{
+	int h, l;
+
+	for (h = 0; h < 10; h++)
+	{
+		for (l =1; l < 10; l++)
+		{
+			putchar(h + '0');
+			putchar(l + '0');
+	
+		if (h != 9 || l != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
+
