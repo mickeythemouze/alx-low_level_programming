b438ac1e439e6d7578c445e746c918698cc924e8
Oluwagbemide_Fikayo <d.fikayo@yahoo.com>
2023-06-16 17:21:07 -0700

    Base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,12 +12,12 @@ int main(void)
 	int ch;
 
 	for (ch = 0; ch < 10; ch++)
-		/*First 9 numbers in the hexadecimal 0-9*/
+#First 9 numbers in the hexadecimal 0-9
 		{
 			putchar(ch + '0');
 		}
 	for (ch = 0; ch < 6; ch++)
-		/*Last 5 number in hexidecimal a-e*/
+#Last 5 number in hexidecimal a-e
 		{
 			putchar(ch + 'a');
 		}
