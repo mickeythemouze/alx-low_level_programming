3e13dfce8f52a9746d2ca697870496dc2056adad
Oluwagbemide_Fikayo <d.fikayo@yahoo.com>
2023-06-16 17:57:57 -0700

    prints infite comb of single digit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,17 +13,14 @@ int main(void)
 
 	for (h = 0; h < 10; h++)
 	{
-		for (l =1; l < 10; l++)
+		for (l = 1; l < 10; l++)
 		{
 			putchar(h + '0');
-			putchar(l + '0');
-	
-		if (h != 9 || l != 9)
-		{
 			putchar(',');
 			putchar(' ');
+			putchar(l + '0');
+	
 		}
-	}
 	}
 	return (0);
 }
