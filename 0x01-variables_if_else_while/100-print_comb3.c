330fb4217c9933a3826e338a11adeda1b4900dba
Oluwagbemide_Fikayo <d.fikayo@yahoo.com>
2023-06-16 19:11:16 -0700

    100 and more

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+/*prints all possible different combinations of two digits*/
+int main(void)
+{
+	int f, k;
+
+	for (f = 0; f < 9; f++)
+	{
+		for (k = f + 1; k < 10; k++)
+		{
+			putchar(f + '0');
+			putchar(k + '0');
+		}
+		if ( f != 8 || k != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
