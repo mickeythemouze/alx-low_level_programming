cd31404cd306c4824b28b00233b5b29211b5f12d
Oluwagbemide_Fikayo <d.fikayo@yahoo.com>
2023-06-19 15:39:21 -0700

    hopeful

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,11 +14,11 @@ int main(void)
 	float e;
 
 	/*size of variable types*/
-	printf("Size of a char: %lu byte(s)\n",sizeof(j));
-	printf("Size of an int: %lu byte(s)\n",sizeof(b));
+	printf("Size of a char: %lu byte(s)\n", sizeof(j));
+	printf("Size of an int: %lu byte(s)\n", sizeof(b));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(c));
-	printf("Size of a long long int: %lu byte(s)", sizeof(d));
-	printf("Size of a float: %lu byte(s)\n", sizeof(e));		
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
+	printf("Size of a float: %lu byte(s)\n", sizeof(e));
 	return (0);
 
 }
