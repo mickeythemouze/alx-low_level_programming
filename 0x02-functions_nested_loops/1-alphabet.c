96fd048fe75471f888d2a6f09334b6baf8cef096
Oluwagbemide_Fikayo <d.fikayo@yahoo.com>
2023-06-21 04:33:58 -0700

    Printing alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - a function that prints the alphabet, in lowercase
+ */
+void print_alphabet(void)
+{
+	char r;
+
+	for (r = 'a'; r <= 'z'; r++)
+		_putchar('r');
+
+	_putchar('\n');
+}
