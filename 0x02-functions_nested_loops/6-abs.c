686dc6a3a3eaf3a64f4229b9185115613da522b6
Oluwagbemide_Fikayo <d.fikayo@yahoo.com>
2023-06-23 07:27:23 -0700

    update

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * Return: Always 0 (Success)
+ */
+int _abs(int j)
+{
+	if (j < 0)
+	{
+	return (-j);
+	}
+	else
+	{
+		return (j);
+	}
+}
