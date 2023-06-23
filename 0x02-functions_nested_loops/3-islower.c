766b5d7d03347837ba73ec575633f21575bbbb28
Oluwagbemide_Fikayo <d.fikayo@yahoo.com>
2023-06-23 05:09:31 -0700

    hard

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase charcter
+ *
+ * c - lowercase characters
+ *
+ * Return: 1 if its lowercase and 0 if otherwise
+ */
+int _islower(int c)
+{
+	int c;
+
+	if (c >= 'a' && c <= 'z')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
