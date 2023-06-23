5bb221228b08d900b477c496f3eb1dc0adcb5f9e
Oluwagbemide_Fikayo <d.fikayo@yahoo.com>
2023-06-23 09:18:33 -0700

    update

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ *
+ * starting from 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+	int f, j;
+
+	f = 0;
+		while(f < 24)
+		{
+			j = 0;
+			while(j < 60)
+			{
+				_putchar((f / 10) + '0');
+				_putchar((f % 10) + '0');
+				_putchar(':');
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
+				_putchar('\n');
+				j++;
+			}
+			f++;
+		}
+}
