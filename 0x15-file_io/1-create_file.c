78c809c6c74f0409b87f2ca179c5477e64b4a854
root <d.fikayo@yahoo.com>
2023-09-04 15:26:07 -0700

    night time betty stress

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 		for (ln = 0; text_content[ln];)
 			ln++;
 	}
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC , 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(fd, text_content, ln);
 	if (fd == -1 || w == -1)
 		return (-1);
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,7 +14,7 @@ char *create_buffer(char *file)
 {
 	char *buffer;
 
-	buffer = malloc(sizeof(char) *1024);
+	buffer = malloc(sizeof(char) * 1024);
 	if (buffer == NULL)
 	{
 		dprintf(STDERR_FILENO,
@@ -85,8 +85,7 @@ int main(int argc, char *argv[])
 		}
 		r = read(from, buffer, 1024);
 		to = open(argv[2], O_WRONLY | O_APPEND);
-	}
-	while (r > 0);
+	} while (r > 0);
 	free(buffer);
 	close_file(from);
 	close_file(to);
